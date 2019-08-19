8bff3c5bdc577f83439d0f9cdc6dd716a6a8010b
tessai9 <lon1ago9@yahoo.co.jp>
2019-08-20 00:37:06 +0900

    get timeline component

---
+++ app/javascript/tweets.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-center">
+    <ul v-for:>
+    </ul>
+  </div>
+</template>
+
+<script>
+const axios = require('axios')
+
+export default {
+  name: 'Tweets',
+  data: function() {
+    return {
+      error_flg: false,
+      error_message: "今日はゆっくり休もうぜ（エラーが起きました）",
+      get_data: []
+    }
+  },
+  created:
+    async function() {
+      await axios
+        .get('/twitter')
+        .then(res => function() {
+          console.log(res)
+        })
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
