df0227c54ebaf08a22dbc82f97fceff314bcd04b
tessai9 <lon1ago9@yahoo.co.jp>
2019-08-19 21:18:57 +0900

    set api key info on credentials

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,12 +2,12 @@ class TwitterController < ApplicationController
 
   def index
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = Rails.application.secrets.twitter_consumer_key
-      config.consumer_secret     = Rails.application.secrets.twitter_consumer_secret
-      config.access_token        = Rails.application.secrets.twitter_access_token
-      config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+      config.consumer_key        = Rails.application.credentials.twitter_consumer_key
+      config.consumer_secret     = Rails.application.credentials.twitter_consumer_secret
+      config.access_token        = Rails.application.credentials.twitter_access_token
+      config.access_token_secret = Rails.application.credentials.twitter_access_token_secret
     end
-    render :json => JSON.generate(@client.search("#有言実GO -RT", lang: "ja").first.text)
+    render :json => JSON.generate(@client.search("#有言実GO -RT", lang: "ja").take(5).text)
   end
 
 end
