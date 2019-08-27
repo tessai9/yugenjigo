d70bbbba30d7015d913ffaac30608597302a4703
tessai9 <lon1ago9@yahoo.co.jp>
2019-08-28 01:44:42 +0900

    get tweets and display as a card

--- app/javascript/tweets.vue
+++ app/javascript/tweets.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="text-center">
-    <ul v-for:>
-    </ul>
-  </div>
+  <b-container id="other_declaration" class="p-0" fluid>
+    <h4>みんなの宣言</h4>
+    <b-card-group v-if="!error_flg" class="tweet_group" deck>
+      <b-card
+        v-for="tweet in get_data"
+        class="tweet my-1"
+        border-variant="primary"
+        :header="tweet.user.name"
+        header-bg-variant="default"
+        align="center"
+      >
+        <b-card-text>{{tweet.text}}</b-card-text>
+      </b-card>
+    </b-card-group>
+    <p v-else>{{error_message}}</p>
+  </b-container>
 </template>
 
 <script>
@@ -14,19 +26,40 @@ export default {
     return {
       error_flg: false,
       error_message: "今日はゆっくり休もうぜ（エラーが起きました）",
-      get_data: []
+      get_data: [],
+      num_of_tweets: 0
     }
   },
   created:
-    async function() {
-      await axios
-        .get('/twitter')
-        .then(res => function() {
-          console.log(res)
+    function() {
+      const self = this
+      axios.get('/twitter')
+        .then(function(res){
+          if (!res.statusText == "OK"){
+            self.error_flg = true
+          }
+          self.get_data = res.data.statuses
+          self.num_of_tweets = res.data.length
         })
     }
 }
 </script>
 
 <style lang="scss" scoped>
+#other_declaration {
+  max-width: 75%;
+}
+
+.tweet_group {
+  flex-flow: row wrap;
+  display: flex;
+  .tweet {
+    font-size: 0.7em;
+    text-overflow: ellipsis;
+    display: inline-block;
+    min-width: 14rem;
+    max-width: 14rem;
+  }
+}
+
 </style>
